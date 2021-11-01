c92762ead6c7ee0c5c0391dd83239e18264e75f2
vit81g <spellhvit@gmail.com>
2021-11-01 15:10:32 +0300

    hw10

--- homework.py
+++ homework.py
@@ -105,7 +105,10 @@ class Swimming(Training):
 
     def get_mean_speed(self) -> float:
         """Получить среднюю скорость движения."""
-        return self.length_pool * self.count_pool / self.M_IN_KM / self.duration
+        return (self.length_pool
+                * self.count_pool
+                / self.M_IN_KM
+                / self.duration)
 
     def get_spent_calories(self) -> float:
         """Получить количество затраченных калорий."""
